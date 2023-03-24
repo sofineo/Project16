72c13d0fb5c052bf940f057492445583993c0591
Sofia Neo <sofineo@gmail.com>
2023-03-24 18:47:50 -0300

    route update user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,5 +6,6 @@ const UserControllers = require("../controllers/users.controllers")
 const userControllers = new UserControllers()
 
 usersRouter.post("/", userControllers.create)
+usersRouter.put("/:id", userControllers.update)
 
 module.exports = usersRouter
